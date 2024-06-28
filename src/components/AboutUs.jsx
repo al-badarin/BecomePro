00522760f726f74b27d94f07ffe9af944b87f290
al-badarin <jamalalbadarin@gmail.com>
2024-06-28 22:18:23 +0300

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,72 @@
+export default function AboutUs() {
+  return (
+    <section className="us_section layout_padding">
+      <div className="container">
+        <div className="heading_container">
+          <h2>Why Choose Us</h2>
+        </div>
+
+        <div className="us_container ">
+          <div className="row">
+            <div className="col-lg-3 col-md-6">
+              <div className="box">
+                <div className="img-box">
+                  <img src="images/u-1.png" alt="" />
+                </div>
+                <div className="detail-box">
+                  <h5>QUALITY EQUIPMENT</h5>
+                  <p>
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div className="box">
+                <div className="img-box">
+                  <img src="images/u-4.png" alt="" />
+                </div>
+                <div className="detail-box">
+                  <h5>NUTRITION</h5>
+                  <p>
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div className="box">
+                <div className="img-box">
+                  <img src="images/u-2.png" alt="" />
+                </div>
+                <div className="detail-box">
+                  <h5>HEALTHY DIET PLAN</h5>
+                  <p>
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div className="box">
+                <div className="img-box">
+                  <img src="images/u-3.png" alt="" />
+                </div>
+                <div className="detail-box">
+                  <h5>SPORT TRAINING</h5>
+                  <p>
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
