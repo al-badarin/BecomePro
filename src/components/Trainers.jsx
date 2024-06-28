6d3fd3ba6e45f71f6039a55bcef6e69002eef74d
al-badarin <jamalalbadarin@gmail.com>
2024-06-28 22:18:35 +0300

    Create Trainers.jsx

---
+++ src/components/Trainers.jsx
@@ -0,0 +1,76 @@
+export default function Trainers() {
+  return (
+    <section className="trainer_section layout_padding">
+      <div className="container">
+        <div className="heading_container">
+          <h2>Our Gym Trainers</h2>
+        </div>
+        <div className="row">
+          <div className="col-lg-4 col-md-6 mx-auto">
+            <div className="box">
+              <div className="name">
+                <h5>Smirth Jon</h5>
+              </div>
+              <div className="img-box">
+                <img src="images/t1.jpg" alt="" />
+              </div>
+              <div className="social_box">
+                <a href="">
+                  <img src="images/facebook-logo.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/twitter.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/instagram-logo.png" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 mx-auto">
+            <div className="box">
+              <div className="name">
+                <h5>Yordan Zhelev</h5>
+              </div>
+              <div className="img-box">
+                <img src="images/t2.jpg" alt="" />
+              </div>
+              <div className="social_box">
+                <a href="">
+                  <img src="images/facebook-logo.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/twitter.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/instagram-logo.png" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 mx-auto">
+            <div className="box">
+              <div className="name">
+                <h5>Alex Den</h5>
+              </div>
+              <div className="img-box">
+                <img src="images/t3.jpg" alt="" />
+              </div>
+              <div className="social_box">
+                <a href="">
+                  <img src="images/facebook-logo.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/twitter.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/instagram-logo.png" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
