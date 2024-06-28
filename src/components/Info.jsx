311b50b55ff81dbe74e2098bd1eb90acf601fd9c
al-badarin <jamalalbadarin@gmail.com>
2024-06-28 22:18:33 +0300

    Create Info.jsx

---
+++ src/components/Info.jsx
@@ -0,0 +1,40 @@
+export default function Info() {
+  return (
+    <section className="info_section layout_padding2">
+      <div className="container">
+        <div className="info_items">
+          <a href="">
+            <div className="item ">
+              <div className="img-box box-1">
+                <img src="" alt="" />
+              </div>
+              <div className="detail-box">
+                <p>Location</p>
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="item ">
+              <div className="img-box box-2">
+                <img src="" alt="" />
+              </div>
+              <div className="detail-box">
+                <p>+02 1234567890</p>
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div className="item ">
+              <div className="img-box box-3">
+                <img src="" alt="" />
+              </div>
+              <div className="detail-box">
+                <p>demo@gmail.com</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
