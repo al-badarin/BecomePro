66b7692b516310002acd2edc488cbd0742d38cbf
al-badarin <jamalalbadarin@gmail.com>
2024-06-28 22:18:31 +0300

    Create Health.jsx

---
+++ src/components/Health.jsx
@@ -0,0 +1,30 @@
+export default function Health() {
+  return (
+    <section className="heathy_section layout_padding">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 mx-auto">
+            <div className="detail-box">
+              <h2>HEALTHY MIND, HEALTHY BODY</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillumLorem ipsum dolor
+                sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum
+              </p>
+              <div className="btn-box">
+                <a href="">READ MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
