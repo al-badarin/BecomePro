fc572518a23ee6fc33de292e270df58a3f15793d
al-badarin <jamalalbadarin@gmail.com>
2024-06-30 19:28:03 +0300

    add new photos of coaches

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -3,16 +3,38 @@ export default function Trainers() {
     <section className="trainer_section layout_padding">
       <div className="container">
         <div className="heading_container">
-          <h2>Our Gym Trainers</h2>
+          <h2>Our Coaches</h2>
         </div>
         <div className="row">
           <div className="col-lg-4 col-md-6 mx-auto">
             <div className="box">
               <div className="name">
-                <h5>Smirth Jon</h5>
+                <h5>Dimitar Ivanov</h5>
               </div>
               <div className="img-box">
-                <img src="images/t1.jpg" alt="" />
+                <img src="images/mitko_cska.jpg" alt="Trainer Photo - Dimitar" />
+              </div>
+              <div className="social_box">
+                <a href="">
+                  <img src="images/facebook-logo.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/twitter.png" alt="" />
+                </a>
+                <a href="">
+                  <img src="images/instagram-logo.png" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mx-auto">
+            <div className="box">
+              <div className="name">
+                <h5>Jamal Al Badarin</h5>
+              </div>
+              <div className="img-box">
+                <img src="images/jim-ball-up.JPEG" alt="Trainer Photo - Jamal" />
               </div>
               <div className="social_box">
                 <a href="">
@@ -33,28 +55,7 @@ export default function Trainers() {
                 <h5>Yordan Zhelev</h5>
               </div>
               <div className="img-box">
-                <img src="images/t2.jpg" alt="" />
-              </div>
-              <div className="social_box">
-                <a href="">
-                  <img src="images/facebook-logo.png" alt="" />
-                </a>
-                <a href="">
-                  <img src="images/twitter.png" alt="" />
-                </a>
-                <a href="">
-                  <img src="images/instagram-logo.png" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6 mx-auto">
-            <div className="box">
-              <div className="name">
-                <h5>Alex Den</h5>
-              </div>
-              <div className="img-box">
-                <img src="images/t3.jpg" alt="" />
+                <img src="images/dancho-official.jpg" alt="Trainer Photo - Yordan" />
               </div>
               <div className="social_box">
                 <a href="">
