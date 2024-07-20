3d5af12228dcf25721badcca501adb1d53cbe734
al-badarin <jamalalbadarin@gmail.com>
2024-07-21 01:00:59 +0300

    Update Home.jsx

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,5 +1,14 @@
+import ArticleItem from '../article-catalog/article-item/ArticleItem';
+
 export default function Home() {
-    return(
-        <h1>Latest Articles</h1>
-    )
+  return (
+    <>
+      <h1>Latest Articles</h1>
+      
+      // TODO: - render all articles one by one; - use cards to display
+      <ArticleItem />
+      <ArticleItem />
+      <ArticleItem />
+    </>
+  );
 }
