4bd9df78e9d25999f4b651f55373b3f7ccb38d1a
al-badarin <jamalalbadarin@gmail.com>
2024-07-21 01:00:50 +0300

    add Logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import HeroSlider from './components/hero-slider/HeroSlider';
+import ArticleCatalog from './components/article-catalog/ArticleCatalog';
 import AboutUs from './components/about-us/AboutUs';
 import Trainers from './components/trainers/Trainers';
 import Contact from './components/contact/Contact';
-// import Health from "./components/Health";
-// import Info from "./components/Info";
 import Login from './components/login/Login';
-import Footer from './components/footer/Footer';
 import Register from './components/register/Register';
-import HeroSlider from './components/hero-slider/HeroSlider';
+import Logout from './components/logout/Logout';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -26,11 +26,13 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<ArticleCatalog />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/trainers" element={<Trainers />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </main>
 
