80ef1e9895dbfdff53981df73a432adf43cc5a94
al-badarin <jamalalbadarin@gmail.com>
2024-07-21 00:06:43 +0300

    add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,171 +1,192 @@
-import Hero from "./components/Hero";
-import AboutUs from "./components/AboutUs";
-import Health from "./components/Health";
-import Trainers from "./components/Trainers";
-import Contact from "./components/Contact";
-import Info from "./components/Info";
-import Footer from "./components/Footer";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+import Header from './components/header/Header';
+// import AboutUs from "./components/AboutUs";
+// import Health from "./components/Health";
+// import Trainers from "./components/Trainers";
+// import Contact from "./components/Contact";
+// import Info from "./components/Info";
+import Footer from './components/Footer';
+import Home from './components/home/Home';
+// import Login from './components/login/Login';
+
+const App = () => {
   return (
-    <div>
-      <Hero />
-      
-      //TODO: (slider_section position-relative) is not showing
-      <section className="slider_section position-relative">
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-ride="carousel"
-        >
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className="container">
-                <div className="col-lg-10 col-md-11 mx-auto">
-                  <div className="detail-box">
-                    <div>
-                      <h3>Fitness</h3>
-                      <h2>Training</h2>
-                      <h1>Neogym</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse .
-                      </p>
-                      <div className="">
-                        <a href="">Contact Us</a>
+    <>
+      <div class="hero_area">
+        <Header />
+
+        <section class=" slider_section position-relative">
+          <div
+            id="carouselExampleIndicators"
+            class="carousel slide"
+            data-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="container">
+                  <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="detail-box">
+                      <div>
+                        <h3>Fitness</h3>
+                        <h2>Training</h2>
+                        <h1>Neogym</h1>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat. Duis aute irure dolor in
+                          reprehenderit in voluptate velit esse .
+                        </p>
+                        <div class="">
+                          <a href="">Contact Us</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container">
+                  <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="detail-box">
+                      <div>
+                        <h3>Fitness</h3>
+                        <h2>Training</h2>
+                        <h1>Neogym</h1>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat. Duis aute irure dolor in
+                          reprehenderit in voluptate velit esse .
+                        </p>
+                        <div class="">
+                          <a href="">Contact Us</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container">
+                  <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="detail-box">
+                      <div>
+                        <h3>Fitness</h3>
+                        <h2>Training</h2>
+                        <h1>Neogym</h1>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat. Duis aute irure dolor in
+                          reprehenderit in voluptate velit esse .
+                        </p>
+                        <div class="">
+                          <a href="">Contact Us</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container">
+                  <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="detail-box">
+                      <div>
+                        <h3>Fitness</h3>
+                        <h2>Training</h2>
+                        <h1>Neogym</h1>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat. Duis aute irure dolor in
+                          reprehenderit in voluptate velit esse .
+                        </p>
+                        <div class="">
+                          <a href="">Contact Us</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container">
+                  <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="detail-box">
+                      <div>
+                        <h3>Fitness</h3>
+                        <h2>Training</h2>
+                        <h1>Neogym</h1>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat. Duis aute irure dolor in
+                          reprehenderit in voluptate velit esse .
+                        </p>
+                        <div class="">
+                          <a href="">Contact Us</a>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="carousel-item">
-              <div className="container">
-                <div className="col-lg-10 col-md-11 mx-auto">
-                  <div className="detail-box">
-                    <div>
-                      <h3>Fitness</h3>
-                      <h2>Training</h2>
-                      <h1>Neogym</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse .
-                      </p>
-                      <div className="">
-                        <a href="">Contact Us</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="container">
-                <div className="col-lg-10 col-md-11 mx-auto">
-                  <div className="detail-box">
-                    <div>
-                      <h3>Fitness</h3>
-                      <h2>Training</h2>
-                      <h1>Neogym</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse .
-                      </p>
-                      <div className="">
-                        <a href="">Contact Us</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="container">
-                <div className="col-lg-10 col-md-11 mx-auto">
-                  <div className="detail-box">
-                    <div>
-                      <h3>Fitness</h3>
-                      <h2>Training</h2>
-                      <h1>Neogym</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse .
-                      </p>
-                      <div className="">
-                        <a href="">Contact Us</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="container">
-                <div className="col-lg-10 col-md-11 mx-auto">
-                  <div className="detail-box">
-                    <div>
-                      <h3>Fitness</h3>
-                      <h2>Training</h2>
-                      <h1>Neogym</h1>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse .
-                      </p>
-                      <div className="">
-                        <a href="">Contact Us</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ol class="carousel-indicators">
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="0"
+                class="active"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="1"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="2"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="3"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="4"
+              ></li>
+            </ol>
           </div>
-          <ol className="carousel-indicators">
-            <li
-              data-target="#carouselExampleIndicators"
-              data-slide-to={0}
-              className="active"
-            />
-            <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-            <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-            <li data-target="#carouselExampleIndicators" data-slide-to={3} />
-            <li data-target="#carouselExampleIndicators" data-slide-to={4} />
-          </ol>
-        </div>
-      </section>
-      <AboutUs/>
+        </section>
+      </div>
 
-      <Trainers/>
-
-      <Health/>
-
-      <Contact/>
-
-      <Info/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/register" component={Register} />
+          <Route path="/articles" exact component={Articles} />
+          <Route path="/articles/:id" component={ArticleDetails} />
+          <Route path="/training-programs" exact component={TrainingPrograms} />
+          <Route path="/training-programs/:id" component={TrainingProgramDetails} /> */}
+        </Routes>
+      </main>
 
       <Footer />
-    </div>
+    </>
   );
-}
+};
 
 export default App;
