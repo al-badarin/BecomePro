a5e6e1058a7c4ea62cb30896cd1ce3fe456d4781
al-badarin <jamalalbadarin@gmail.com>
2024-07-21 20:05:09 +0300

    add Login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,3 +1,42 @@
 export default function Login() {
-  return <h1>LOGIN FORM</h1>;
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h3>Login</h3>
+            </div>
+            <div className="card-body">
+              <form >
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary mt-3">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
