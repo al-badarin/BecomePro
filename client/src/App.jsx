52e346b1b1763f6dac61e1e8a12b67e85b304a70
al-badarin <jamalalbadarin@gmail.com>
2024-07-22 15:38:48 +0300

    update authContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-
-import * as authService from './services/authService';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/home/Home';
 import Header from './components/header/Header';
@@ -16,72 +13,11 @@ import Register from './components/register/Register';
 import Logout from './components/logout/Logout';
 import Footer from './components/footer/Footer';
 
-import AuthProvider from './contexts/authContext';
+import { AuthProvider } from './contexts/authContext';
 
 const App = () => {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem('accessToken');
-
-    return {};
-  });
-  const [error, setError] = useState(null);
-
-  const loginSubmitHandler = async (values) => {
-    try {
-      const result = await authService.login(values.email, values.password);
-
-      setAuth(result);
-
-      localStorage.setItem('accessToken', result.accessToken);
-
-      navigate('/');
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  const registerSubmitHandler = async (values) => {
-    if (values.password !== values.repassword) {
-      setError('Passwords do not match');
-
-      return;
-    }
-
-    try {
-      const result = await authService.register(
-        values.email,
-        values.username,
-        values.password
-      );
-
-      setAuth(result);
-
-      localStorage.setItem('accessToken', result.accessToken);
-
-      navigate('/');
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  const logoutHandler = () => {
-    setAuth({});
-
-    localStorage.removeItem('accessToken');
-  };
-
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  };
-
   return (
-    <AuthProvider value={values}>
+    <AuthProvider>
       <div className="hero_area">
         <Header />
 
--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,9 +1,73 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+import { useNavigate } from 'react-router';
+
+import * as authService from '../services/authService';
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({ children, value }) => {
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState(() => {
+    localStorage.removeItem('accessToken');
+
+    return {};
+  });
+  const [error, setError] = useState(null);
+
+  const loginSubmitHandler = async (values) => {
+    try {
+      const result = await authService.login(values.email, values.password);
+
+      setAuth(result);
+
+      localStorage.setItem('accessToken', result.accessToken);
+
+      navigate('/');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const registerSubmitHandler = async (values) => {
+    if (values.password !== values.repassword) {
+      setError('Passwords do not match');
+
+      return;
+    }
+
+    try {
+      const result = await authService.register(
+        values.email,
+        values.username,
+        values.password
+      );
+
+      setAuth(result);
+
+      localStorage.setItem('accessToken', result.accessToken);
+
+      navigate('/');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const logoutHandler = () => {
+    setAuth({});
+
+    localStorage.removeItem('accessToken');
+  };
+
+  const values = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    logoutHandler,
+    username: auth.username,
+    email: auth.email,
+    isAuthenticated: !!auth.accessToken,
+  };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
 AuthContext.displayName = 'AuthContext';
