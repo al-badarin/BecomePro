0bd1fc2c66c1d3aff7e226dca5fcfa37d3578812
al-badarin <jamalalbadarin@gmail.com>
2024-07-23 15:39:23 +0300

    update img to imageUrl

--- client/src/components/article-catalog/article-item/ArticleItem.jsx
+++ client/src/components/article-catalog/article-item/ArticleItem.jsx
@@ -1,10 +1,10 @@
 import styles from './ArticleItem.module.css';
 
-export default function ArticleItem({ title, img, content }) {
+export default function ArticleItem({ title, imageUrl, content }) {
   return (
     <div className={styles.article}>
       <h2 className={styles.title}>{title}</h2>
-      <img className={styles.image} src={img} alt={title} />
+      <img className={styles.image} src={imageUrl} alt={title} />
       <p className={styles.content}>{content}</p>
     </div>
   );
