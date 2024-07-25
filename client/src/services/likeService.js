2e2305301df0204dc5e28d573a0494f726dee2fc
al-badarin <jamalalbadarin@gmail.com>
2024-07-25 16:14:00 +0300

    Create likeService.js

---
+++ client/src/services/likeService.js
@@ -0,0 +1,23 @@
+import * as request from '../lib/request';
+
+const baseUrl = 'http://localhost:3030/data/likes';
+
+export const like = async (articleId) => {
+  return request.post(baseUrl, { articleId });
+};
+
+export const unlike = async (likeId) => {
+  return request.remove(`${baseUrl}/${likeId}`);
+};
+
+// todo:
+export const getLikes = async (articleId) => {
+  return request.get(`${baseUrl}?where=articleId%3D"${articleId}"&count`);
+};
+
+// todo:
+export const userHasLiked = async (articleId, userId) => {
+  return request.get(
+    `${baseUrl}?where=articleId%3D"${articleId}"%20and%20_ownerId%3D"${userId}"`
+  );
+};
