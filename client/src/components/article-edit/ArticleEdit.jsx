4ffe1e83e2e3685e68e28971b308e9d45f18d2ab
al-badarin <jamalalbadarin@gmail.com>
2024-07-25 12:56:31 +0300

    owner of an article can edit

--- client/src/components/article-edit/ArticleEdit.jsx
+++ client/src/components/article-edit/ArticleEdit.jsx
@@ -1,29 +1,33 @@
 import { useNavigate, useParams } from 'react-router';
 import * as articleService from '../../services/articleService';
 import styles from './ArticleEdit.module.css';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import AuthContext from '../../contexts/authContext';
 
 export default function ArticleEdit() {
-  const { articleId } = useParams();
   const navigate = useNavigate();
+  const { userId } = useContext(AuthContext);
+  const { articleId } = useParams();
   const [editFormData, setEditFormData] = useState({
     title: '',
     content: '',
     imageUrl: '',
   });
+  const [isOwner, setIsOwner] = useState(false);
 
   useEffect(() => {
     articleService
       .getOne(articleId)
       .then((article) => {
         setEditFormData(article);
+        setIsOwner(article._ownerId === userId);
       })
       .catch((err) => {
         console.error(err);
         // todo:
         // navigate('/not-found');
       });
-  }, [articleId, navigate]);
+  }, [articleId, navigate, userId]);
 
   const handleChange = (e) => {
     setEditFormData({
@@ -35,6 +39,11 @@ export default function ArticleEdit() {
   const editArticleSubmitHandler = async (e) => {
     e.preventDefault();
 
+    if (!isOwner) {
+      alert('You are not authorized to edit this article.');
+      return;
+    }
+
     try {
       await articleService.edit(articleId, editFormData);
       navigate(`/articles/${articleId}`);
@@ -42,6 +51,11 @@ export default function ArticleEdit() {
       console.error(error);
     }
   };
+
+  //   TODO: style the <p>
+  if (!isOwner) {
+    return <p>You are not authorized to edit this article.</p>;
+  }
 
   return (
     <div className={styles.articleEdit}>
