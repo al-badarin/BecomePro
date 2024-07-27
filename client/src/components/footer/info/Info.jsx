ef29a7f247d795f23584db4d563320f3f2c66a48
al-badarin <jamalalbadarin@gmail.com>
2024-07-27 16:45:50 +0300

    styled Info

--- client/src/components/footer/info/Info.jsx
+++ client/src/components/footer/info/Info.jsx
@@ -1,44 +1,30 @@
-import { Link } from 'react-router-dom';
+import styles from './Info.module.css';
 
 export default function Info() {
-  // TODO: add paths to LINKS
-
   return (
-    <section className="info_section layout_padding2">
+    <section className={`info_section ${styles.layout_padding2}`}>
       <div className="container">
         <div className="info_items">
-          <Link to="">
-            <div className="item ">
-              <div className="img-box box-1">
-                <img src="" alt="" />
-              </div>
-              <div className="detail-box">
-                <p>Bulgaria</p>
-              </div>
+          <div className="item ">
+            <div className="img-box box-1"></div>
+            <div className="detail-box">
+              <p>Bulgaria</p>
             </div>
-          </Link>
+          </div>
 
-          <Link to="">
-            <div className="item ">
-              <div className="img-box box-2">
-                <img src="" alt="" />
-              </div>
-              <div className="detail-box">
-                <p>+359 888881476</p>
-              </div>
+          <div className="item ">
+            <div className="img-box box-2"></div>
+            <div className="detail-box">
+              <p>+359 888881476</p>
             </div>
-          </Link>
+          </div>
 
-          <Link to="">
-            <div className="item ">
-              <div className="img-box box-3">
-                <img src="" alt="" />
-              </div>
-              <div className="detail-box">
-                <p>become.pro2024@gmail.com</p>
-              </div>
+          <div className="item ">
+            <div className="img-box box-3"></div>
+            <div className="detail-box">
+              <p>become.pro2024@gmail.com</p>
             </div>
-          </Link>
+          </div>
         </div>
       </div>
     </section>
