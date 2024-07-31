ac24b81c654a10617b930053b8042662fe9807b1
al-badarin <jamalalbadarin@gmail.com>
2024-07-31 15:31:54 +0300

    styled ArticleItem

--- client/src/components/article-catalog/article-item/ArticleItem.jsx
+++ client/src/components/article-catalog/article-item/ArticleItem.jsx
@@ -4,13 +4,16 @@ import styles from './ArticleItem.module.css';
 export default function ArticleItem({ _id, title, imageUrl, content }) {
   return (
     <div className={styles.article}>
-      <h2 className={styles.title}>{title}</h2>
-      <img className={styles.image} src={imageUrl} alt={title} />
-      <p className={styles.content}>{content}</p>
-
-      <Link to={`/articles/${_id}`} className={styles.detailsButton}>
-        Read More
-      </Link>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={imageUrl} alt={title} />
+      </div>
+      <div className={styles.textContainer}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.content}>{content}</p>
+        <Link to={`/articles/${_id}`} className={styles.detailsButton}>
+          Read More
+        </Link>
+      </div>
     </div>
   );
 }
