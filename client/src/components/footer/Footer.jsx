07fc72ab2cc1c3bc55fd0833dc1cf13d0f5b2a88
al-badarin <jamalalbadarin@gmail.com>
2024-08-01 21:57:50 +0300

    add styles t the footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import Info from './info/Info';
 
+import styles from './Footer.module.css';
+
 export default function Footer() {
   return (
     <>
       <Info />
-
-      <footer className="container-fluid footer_section">
+      <footer className={`container-fluid ${styles.footer_section}`}>
         <p>All Rights Reserved. &copy; BecomePro 2024</p>
       </footer>
     </>
