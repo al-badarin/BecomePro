78a8697ec8a072e3cd376c81974cbf7b8b9660d0
al-badarin <jamalalbadarin@gmail.com>
2024-08-04 13:27:05 +0300

    fix footer hardcoded year

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,11 +3,13 @@ import Info from './info/Info';
 import styles from './Footer.module.css';
 
 export default function Footer() {
+  const currYear = new Date().getFullYear();
+
   return (
     <>
       <Info />
       <footer className={`container-fluid ${styles.footer_section}`}>
-        <p>All Rights Reserved. &copy; BecomePro 2024</p>
+        <p>All Rights Reserved. &copy; BecomePro {currYear}</p>
       </footer>
     </>
   );
