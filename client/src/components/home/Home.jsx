a10b25c4969b2fc4168fda84da270e7354a0c376
al-badarin <jamalalbadarin@gmail.com>
2024-08-05 23:52:36 +0300

    add trainers to home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,9 @@
-import AboutUs from './about-us/AboutUs';
 import HeroSlider from './hero-slider/HeroSlider';
+import AboutUs from '../about-us/AboutUs';
+import Trainers from '../trainers/Trainers';
 import LatestArticlesCarousel from './latest-articles-carousel/LatestArticlesCarousel';
+
+import styles from './Home.module.css';
 
 export default function Home() {
   return (
@@ -9,9 +12,13 @@ export default function Home() {
         <HeroSlider />
       </div>
 
+      <div className={styles.about}>
+        <AboutUs />
+      </div>
+
+      <Trainers />
+
       <LatestArticlesCarousel />
-      
-      <AboutUs />
     </>
   );
 }
